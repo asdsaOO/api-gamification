e9eece0d7f13c3c13f01431f1d4a2c47c019be7d
asdsaOO <borhul7@gmail.com>
2023-09-19 00:01:03 -0400

    segundo commit

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+
+const express = require('express');
+
+
+const app = express();
+PORT=process.env || 8080;
+
+
+
+app.get('/',(req,res)=>{
+    res.send('hola mundo')
+
+
+});
+
 console.log('hola q hacesssss');
+
+app.listen(PORT, ()=>{
+    console.log('servidor corriendo');
+})
